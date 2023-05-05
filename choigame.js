a1086d8248429c760031f116c5edd1dd29784aec
Choigame123 <108588018+Choigame123@users.noreply.github.com>
2023-05-05 17:44:13 +0700

    update choigame.js

--- choigame.js
+++ choigame.js
@@ -4,6 +4,7 @@ String.prototype.isV4UUID = function() {
 String.prototype.isPositiveNumber = function() {
     return this.match(/\d+/)?.[0].length === this.length && +`${this}` < 100;
 }
+
 let global_scope = {},
 outside_depent = [],
 devc = 0;
@@ -366,3 +367,34 @@ coppyToClipboard = (text = JSON.stringify(global_scope, {
 getCheckBoxByID = checkboxid => {
     return _(checkboxid).checked;
 };
+function storageAvailable(type) {
+  let storage;
+  try {
+    storage = window[type];
+    const x = "__storage_test__";
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return (
+      e instanceof DOMException &&
+      // everything except Firefox
+      (e.code === 22 ||
+        // Firefox
+        e.code === 1014 ||
+        // test name field too, because code might not be present
+        e.name === "QuotaExceededError" ||
+        // Firefox
+        e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+      storage &&
+      storage.length !== 0
+    );
+  }
+}
+
+let storage;
+function setup() {
+    if (storageAvailable("localStorage")) {
+        
+    }
+}
